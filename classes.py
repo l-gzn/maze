f8d413697eaa31495395b2b9835de6ac32d5f19a
lgzn <lucageuzaine@hotmail.com>
2025-02-14 07:46:39 -0500

    Initial commit

---
+++ classes.py
@@ -0,0 +1,136 @@
+import random
+from collections import deque
+import pygame
+
+
+class Cell:
+    def __init__(self, row, col, cell_size):
+        self.row = row
+        self.col = col
+        self.cell_size = cell_size
+        self.walls = {"top": True, "right": True, "bottom": True, "left": True}
+        self.visited = False
+
+    def remove_wall(self, other, direction):
+        if direction == "top":
+            self.walls["top"] = False
+            other.walls["bottom"] = False
+        elif direction == "right":
+            self.walls["right"] = False
+            other.walls["left"] = False
+        elif direction == "bottom":
+            self.walls["bottom"] = False
+            other.walls["top"] = False
+        elif direction == "left":
+            self.walls["left"] = False
+            other.walls["right"] = False
+
+    def draw(self, win):
+        x = self.col * self.cell_size
+        y = self.row * self.cell_size
+
+        if self.visited:
+            pygame.draw.rect(win, "purple", (x, y, self.cell_size, self.cell_size))
+
+
+        if self.walls["top"]:
+            pygame.draw.line(win, "white", (x, y), (x+self.cell_size, y), 2)
+        if self.walls["right"]:
+            pygame.draw.line(win, "white", (x+self.cell_size, y), (x+self.cell_size, y+self.cell_size), 2)
+        if self.walls["bottom"]:
+            pygame.draw.line(win, "white", (x+self.cell_size, y+self.cell_size), (x, y+self.cell_size), 2)
+        if self.walls["left"]:
+            pygame.draw.line(win, "white", (x, y+self.cell_size), (x, y), 2)
+
+    def highlight(self, win):
+        x = self.col * self.cell_size
+        y = self.row * self.cell_size
+        pygame.draw.rect(win, "red", (x, y , self.cell_size, self.cell_size))
+
+class Grid:
+    # forme une liste 2D d'objets cell
+    def __init__(self, rows, cols, cell_size, win):
+        self.rows = rows
+        self.cols = cols
+        self.cell_size = cell_size
+        self.win = win
+        self.grid = [[Cell(row, col, cell_size) for col in range(cols)] for row in range(rows)]
+
+        self.stack = deque()
+        self.current_cell = self.grid[random.randint(0, rows-1)][random.randint(0, cols-1)]
+        self.current_cell.visited = True
+        self.stack.append(self.current_cell)
+
+    def get_cell(self, row, col):
+        # pour acceder chaque cell
+        if 0 <= row <= self.rows and 0 <= col <= self.cols:
+            return self.grid[row][col]
+        else:
+            return None
+
+
+    def get_neighbors(self, row, col):
+        neighbors = {}
+        if (top:= self.get_cell(row-1, col)):
+            neighbors["top"] = top
+        if col != self.cols-1:
+            if (right:= self.get_cell(row, col+1)):
+                neighbors["right"] = right
+        if row != self.rows-1:
+            if (bottom:= self.get_cell(row+1, col)):
+                neighbors["bottom"] = bottom
+        if (left:= self.get_cell(row, col-1)):
+            neighbors["left"] = left
+
+        return neighbors
+    
+    def remove_wall_between(self, cell1, cell2):
+        if cell1.row == cell2.row:
+            if cell1.col < cell2.col:
+                cell1.remove_wall(cell2, "right")
+            else:
+                cell1.remove_wall(cell2, "left")
+        elif cell1.col == cell2.col:
+            if cell1.row < cell2.row:
+                cell1.remove_wall(cell2, "bottom")
+            else:
+                cell1.remove_wall(cell2, "top")
+
+    def draw(self, current_cell=None):
+        self.win.fill("black")
+        
+
+        for row in self.grid:
+            for cell in row:
+                if cell.visited:
+                    cell.draw(self.win)
+
+
+        if self.current_cell and self.stack:
+            self.current_cell.highlight(self.win)
+
+        
+        pygame.display.update()
+
+    def maze_gen(self, loops=False):
+        if self.stack:
+            self.current_cell = self.stack.pop()
+            self.current_cell.visited = True
+            neighbors = self.get_neighbors(self.current_cell.row, self.current_cell.col)
+            unvisited_cells = [cell for cell in neighbors.values() if not cell.visited]
+
+            # Petite chance d'enlever des murs supplementaires
+            if loops and random.random() < 0.25:
+                loops  = [cell for cell in neighbors.values() if cell.visited]
+                if loops:
+                    chosen_cell = random.choice(loops)
+                    self.remove_wall_between(self.current_cell, chosen_cell)
+
+            if unvisited_cells:
+                self.stack.append(self.current_cell)
+                chosen_cell = random.choice(unvisited_cells)
+                self.remove_wall_between(self.current_cell, chosen_cell)
+                self.stack.append(chosen_cell)
+
+        else:
+            self.current_cell = None
---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+from classes import Grid
+
+pygame.init()
+
+
+WIDTH = 650
+HEIGHT = 650
+ROWS, COLS = 25, 25
+
+
+CELL_SIZE = WIDTH // COLS
+WIN = pygame.display.set_mode((WIDTH + 2, HEIGHT + 2))
+pygame.display.set_caption("Maze")
+clock = pygame.time.Clock()
+running = True
+
+
+grid = Grid(ROWS, COLS, CELL_SIZE, WIN)
+
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    
+    while grid.current_cell:
+        grid.draw()
+        grid.maze_gen()
+    
+
+
+    clock.tick(120)
+pygame.quit()
